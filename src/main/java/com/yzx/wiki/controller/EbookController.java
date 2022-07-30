1798f1b53a2ef6c6832f72b496ded96cc93efc34
moluranli <2285288446@qq.com>
2022-07-30 17:21:09 +0800

    使用axios调用后端接口,解决跨域问题

--- src/main/java/com/yzx/wiki/controller/EbookController.java
+++ src/main/java/com/yzx/wiki/controller/EbookController.java
@@ -1,17 +1,14 @@
 package com.yzx.wiki.controller;
 
-import com.yzx.wiki.domain.Ebook;
 import com.yzx.wiki.req.EbookReq;
 import com.yzx.wiki.resp.CommonResp;
 import com.yzx.wiki.resp.EbookResp;
 import com.yzx.wiki.service.EbookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.awt.*;
 import java.util.List;
 
 @RestController
